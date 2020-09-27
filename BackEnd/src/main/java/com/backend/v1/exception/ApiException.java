e94fb1ac1265511d2bcc7ca15ce971ed949e689f
KangMyoungGu <kmg1123ck@naver.com>
2020-09-27 15:05:04 +0900

    exception  package

---
+++ BackEnd/src/main/java/com/backend/v1/exception/ApiException.java
@@ -0,0 +1,32 @@
+package com.backend.v1.exception;
+
+
+import com.backend.v1.RtCode;
+
+public class ApiException extends RuntimeException {
+	private static final long serialVersionUID = 7999421329089609170L;
+
+	private RtCode rtCode;
+
+	public ApiException() {
+		super();
+	}
+
+	public ApiException(Exception e) {
+		super(e);
+	}
+
+	public ApiException(RtCode rtCode) {
+		super(rtCode.getErrorMessage());
+		this.rtCode = rtCode;
+	}
+
+	public ApiException(RtCode rtCode, Throwable throwable) {
+		super(rtCode.getErrorMessage(), throwable);
+		this.rtCode = rtCode;
+	}
+
+	public RtCode getRtCode() {
+		return rtCode;
+	}
+}
